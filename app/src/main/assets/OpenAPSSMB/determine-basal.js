05b0fea7a07c55ffdd19c569b090922ebc19b241
D!CKO <dicko72@gmail.com>
2021-03-29 14:43:16 +1100

    fixup! isfboost vs insulinReqPct

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -288,6 +288,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     console.log("UAM_deltaLongRise: " + UAM_deltaLongRise);
     console.log("UAM_deltaAvgRise: " + UAM_deltaAvgRise);
     //MD: Eating now mode for UAM === END
+    var ISFBoostTest = Math.min((1 + Math.max(UAM_deltaAvgRise,0)),profile.EatingNowModeISFBoost);
 
     var sensitivityRatio;
     var high_temptarget_raises_sensitivity = profile.exercise_mode || profile.high_temptarget_raises_sensitivity;
@@ -386,7 +387,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     }
     console.error("; CR:",profile.carb_ratio);
 //    if (eatingnow) sens = sens / profile.EatingNowModeISFBoost; // we want to change the ISF for predictions and initial insulinReq but not the basal to ensure more accurate IOB
-    if (eatingnow) sens = sens / Math.min((1 + Math.max(UAM_deltaAvgRise,0)),profile.EatingNowModeISFBoost); // we want to change the ISF for predictions and initial insulinReq but not the basal to ensure more accurate IOB
+    if (eatingnow) sens = sens / ISFBoostTest; // we want to change the ISF for predictions and initial insulinReq but not the basal to ensure more accurate IOB
 
     sens = autoISF(sens, target_bg, profile, glucose_status, meal_data, autosens_data, sensitivityRatio, eatingnow);
     // compare currenttemp to iob_data.lastTemp and cancel temp if they don't match
