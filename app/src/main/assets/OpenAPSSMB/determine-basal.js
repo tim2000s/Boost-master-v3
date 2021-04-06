b3eeab7866fde16d4b7f3d7c8ba44938395d925c
D!CKO <dicko72@gmail.com>
2021-04-06 19:33:56 +1000

    SMB 0 keep TBR when eating now

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -1282,7 +1282,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
              }
 
             // if insulinReq > 0 but not enough for a microBolus, don't set an SMB zero temp
-            if (insulinReq > 0 && microBolus < profile.bolus_increment || eatingnow && microBolus > 0 && insulinReq - microBolus >= 0) {
+            if (insulinReq > 0 && microBolus < profile.bolus_increment || eatingnow && insulinReq - microBolus >= 0) {
 //            if (insulinReq > 0 && microBolus < profile.bolus_increment) {
                 durationReq = 0;
             }
