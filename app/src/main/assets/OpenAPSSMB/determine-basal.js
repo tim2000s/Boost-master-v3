01337ccb0628ba9b54fe1441e7848f0288c6d8a6
D!CKO <dicko72@gmail.com>
2021-04-10 20:39:57 +1000

    boost_bolus

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -1203,7 +1203,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
                 insulinReqPct = profile.EatingNowInsulinReq; // default % from settings
                 var boostBGthreshold = 162; // 9mmol
-                var boost_scale = round((((eventualBG - boostBGthreshold) + target_bg) / target_bg),2);
+                var boost_scale = round((eventualBG / boostBGthreshold),2);
                 // var boost_scale = round(((eventualBG - target_bg) / target_bg),2);
                 var boost_bolus = round( profile.current_basal * profile.EatingNowbolusboostMinutes / 60 ,2);
                 var UAMBooster = UAMBoost; // this will be the combined boost
@@ -1222,17 +1222,17 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
                 // If we have negative insulin then boost_scale must be needed, add boost_bolus as the prediction is higher than target_bg
 //                insulinReq = round((insulinReq <=0 ? boost_bolus : insulinReq),2);
-                insulinReq = round((insulinReq <=0 ? Math.abs(insulinReq)/2 : insulinReq),2); // lets try this!?
+                insulinReq = (insulinReq <=0 ? Math.abs(insulinReq)/2 : insulinReq); // lets try this!?
 
                 // If we are rising >=0.3
                 if (UAM_safedelta >=5 && UAMBooster >1) {
-                    insulinReq += round((boost_scale > 1 ? boost_bolus : 0),2); // lets try this!?
-                    // Reason is that we boosted, this could be restricted by maxbolus is rise is slowing
-                    UAMBoostReason = " (boost" + (boost_scale >1 ? "+ ":" ") + insulinReq + "*" + UAMBooster + ")";
+                    insulinReq += (boost_scale > 1 ? boost_bolus : 0); // lets try this!?
+                    // Reason is that we boosted, this could be restricted by maxbolus if rise is slowing
+                    UAMBoostReason = " (boost" + (boost_scale >1 ? "+ ":" ") + round(insulinReq,2) + "*" + UAMBooster + ")";
                 } else {
                     // if eventualBG is above target_bg but rising slower or falling restrict insulinReq
                     insulinReqPct = 0.5;
-                    UAMBoostReason = " (limit" + (boost_scale >1 ? "+ ":" ") + insulinReq + "*" + UAMBooster + ")";
+                    UAMBoostReason = " (limit" + (boost_scale >1 ? "+ ":" ") + round(insulinReq,2) + "*" + UAMBooster + ")";
                 }
                 UAMBoostReason += ", UAMBoost " + UAMBoost + ", Boost+ " + boost_scale;
 
