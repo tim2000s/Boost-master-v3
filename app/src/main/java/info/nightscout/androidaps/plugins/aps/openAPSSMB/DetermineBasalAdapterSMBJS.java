60983713a3657ef7c25371da5d5e68577bfc570c
D!CKO <dicko72@gmail.com>
2021-04-11 11:22:18 +1000

    boostBGthreshold

--- app/src/main/java/info/nightscout/androidaps/plugins/aps/openAPSSMB/DetermineBasalAdapterSMBJS.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/aps/openAPSSMB/DetermineBasalAdapterSMBJS.java
@@ -309,6 +309,7 @@ public class DetermineBasalAdapterSMBJS {
         mProfile.put("enableEatingNow", sp.getBoolean(R.string.key_use_eatingnow, false));
         mProfile.put("EatingNowIOB", sp.getDouble(R.string.key_eatingnow_iob, 5));
         mProfile.put("EatingNowUAMBoostMax", sp.getDouble(R.string.key_eatingnow_uamboostpct_max, 1.0));
+        mProfile.put("EatingNowUAMBoostBG", sp.getDouble(R.string.key_eatingnow_boostbg, 0));
         mProfile.put("EatingNowMaxSMBMinutes", sp.getInt(R.string.key_eatingnow_maxbolusminutes, 30));
         mProfile.put("EatingNowbolusboostMinutes", sp.getInt(R.string.key_eatingnow_bolusboostminutes, 30));
         mProfile.put("EatingNowIOBMax", sp.getDouble(R.string.key_eatingnow_iobmax, 0.3));
