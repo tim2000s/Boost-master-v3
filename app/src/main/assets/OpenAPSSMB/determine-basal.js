84f0dcab37b1031fbee7bf2bb166b8bea0d09cb4
dicko <mark@servecold.net>
2021-07-22 12:21:48 +1000

    Increased insulinReqPct for for delta above BG Threshold by 10%

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -1330,7 +1330,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
                     // Restrict insulinReq when above BGBoost_threshold
                     // insulinReqPct = ( bg > BGBoost_threshold ? 0.6 : insulinReqPct );
                     // Restrict insulinReq when above BGBoost_threshold using delta if its not zero
-                    insulinReqPct = ( bg > BGBoost_threshold && insulinReqPct > 0 ? round(Math.max(Math.min(UAM_safedelta/18,1),0.6),2) : insulinReqPct );
+                    insulinReqPct = ( bg > BGBoost_threshold && insulinReqPct > 0 ? round(Math.max(Math.min(UAM_safedelta/18,1),0.7),2) : insulinReqPct );
                     // if BG above threshold with autoISF active and using BGBoost not BGBoost+ then allow 100%
                     if (bg > BGBoost_threshold && typeof liftISF !== 'undefined' && BGBoosted && !UAMBoosted) insulinReqPct = 1.0;
                     SMB_TBR = ( insulinReqPct < 1 ? true : SMB_TBR );
