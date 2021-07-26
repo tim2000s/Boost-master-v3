4c24e9390d8615a3738b10f638edc1d9fe5ab716
Philoul <legoupil.jp@free.fr>
2021-07-26 23:59:02 +0200

    Step 1 creation of Full UAM files

---
+++ app/src/main/java/info/nightscout/androidaps/plugins/aps/fullUAM/FullUAMFragment.kt
@@ -0,0 +1,142 @@
+package info.nightscout.androidaps.plugins.aps.fullUAM
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.android.support.DaggerFragment
+import info.nightscout.androidaps.R
+import info.nightscout.androidaps.databinding.OpenapsamaFragmentBinding
+import info.nightscout.androidaps.logging.AAPSLogger
+import info.nightscout.androidaps.logging.LTag
+import info.nightscout.androidaps.plugins.aps.events.EventOpenAPSUpdateGui
+import info.nightscout.androidaps.plugins.aps.events.EventOpenAPSUpdateResultGui
+import info.nightscout.androidaps.plugins.bus.RxBusWrapper
+import info.nightscout.androidaps.utils.DateUtil
+import info.nightscout.androidaps.utils.FabricPrivacy
+import info.nightscout.androidaps.utils.JSONFormatter
+import info.nightscout.androidaps.utils.resources.ResourceHelper
+import info.nightscout.androidaps.utils.rx.AapsSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.plusAssign
+import org.json.JSONArray
+import org.json.JSONException
+import javax.inject.Inject
+
+class FullUAMFragment : DaggerFragment() {
+
+    private var disposable: CompositeDisposable = CompositeDisposable()
+
+    @Inject lateinit var aapsLogger: AAPSLogger
+    @Inject lateinit var aapsSchedulers: AapsSchedulers
+    @Inject lateinit var rxBus: RxBusWrapper
+    @Inject lateinit var resourceHelper: ResourceHelper
+    @Inject lateinit var fabricPrivacy: FabricPrivacy
+    @Inject lateinit var fullUAMPlugin: FullUAMPlugin
+    @Inject lateinit var dateUtil: DateUtil
+    @Inject lateinit var jsonFormatter: JSONFormatter
+
+    private var _binding: OpenapsamaFragmentBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        _binding = OpenapsamaFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.run.setOnClickListener {
+            fullUAMPlugin.invoke("OpenAPSSMB button", false)
+        }
+    }
+
+    @Synchronized
+    @kotlin.ExperimentalStdlibApi
+    override fun onResume() {
+        super.onResume()
+        disposable += rxBus
+            .toObservable(EventOpenAPSUpdateGui::class.java)
+            .observeOn(aapsSchedulers.main)
+            .subscribe({
+                updateGUI()
+            }, fabricPrivacy::logException)
+        disposable += rxBus
+            .toObservable(EventOpenAPSUpdateResultGui::class.java)
+            .observeOn(aapsSchedulers.main)
+            .subscribe({
+                updateResultGUI(it.text)
+            }, fabricPrivacy::logException)
+
+        updateGUI()
+    }
+
+    @Synchronized
+    override fun onPause() {
+        super.onPause()
+        disposable.clear()
+    }
+
+    @Synchronized
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    @Synchronized
+    @kotlin.ExperimentalStdlibApi
+    fun updateGUI() {
+        if (_binding == null) return
+        fullUAMPlugin.lastAPSResult?.let { lastAPSResult ->
+            binding.result.text = jsonFormatter.format(lastAPSResult.json)
+            binding.request.text = lastAPSResult.toSpanned()
+        }
+        fullUAMPlugin.lastDetermineBasalAdapterUAMJS?.let { determineBasalAdapterUAMJS ->
+            binding.glucosestatus.text = jsonFormatter.format(determineBasalAdapterUAMJS.glucoseStatusParam)
+            binding.currenttemp.text = jsonFormatter.format(determineBasalAdapterUAMJS.currentTempParam)
+            try {
+                val iobArray = JSONArray(determineBasalAdapterUAMJS.iobDataParam)
+                binding.iobdata.text = TextUtils.concat(resourceHelper.gs(R.string.array_of_elements, iobArray.length()) + "\n", jsonFormatter.format(iobArray.getString(0)))
+            } catch (e: JSONException) {
+                aapsLogger.error(LTag.APS, "Unhandled exception", e)
+                @SuppressLint("SetTextI18n")
+                binding.iobdata.text = "JSONException see log for details"
+            }
+
+            binding.profile.text = jsonFormatter.format(determineBasalAdapterUAMJS.profileParam)
+            binding.mealdata.text = jsonFormatter.format(determineBasalAdapterUAMJS.mealDataParam)
+            binding.scriptdebugdata.text = determineBasalAdapterUAMJS.scriptDebug
+            fullUAMPlugin.lastAPSResult?.inputConstraints?.let {
+                binding.constraints.text = it.getReasons(aapsLogger)
+            }
+        }
+        if (fullUAMPlugin.lastAPSRun != 0L) {
+            binding.lastrun.text = dateUtil.dateAndTimeString(fullUAMPlugin.lastAPSRun)
+        }
+        fullUAMPlugin.lastAutosensResult.let {
+            binding.autosensdata.text = jsonFormatter.format(it.json())
+        }
+    }
+
+    @Synchronized
+    private fun updateResultGUI(text: String) {
+        if (_binding == null) return
+        binding.result.text = text
+        binding.glucosestatus.text = ""
+        binding.currenttemp.text = ""
+        binding.iobdata.text = ""
+        binding.profile.text = ""
+        binding.mealdata.text = ""
+        binding.autosensdata.text = ""
+        binding.scriptdebugdata.text = ""
+        binding.request.text = ""
+        binding.lastrun.text = ""
+    }
+}
