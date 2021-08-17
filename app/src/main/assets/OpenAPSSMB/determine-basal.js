c4c8557b354fa320e4aeeddc33942054aeceb732
dicko <mark@servecold.net>
2021-08-17 17:24:30 +1000

    High delta enable eating now with low IOB

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -327,8 +327,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     if (eatingnowPatch && profile.enableUAM && ignoreCOBPatch && eatingnowMaxIOBOK) {
         // enable eatingnow if no TT and safe IOB within safe hours
         if (!profile.temptargetSet && iob_data.iob >= profile.EatingNowIOB && eatingnowtimeOK) eatingnow = true;
-        // enable eating now when there are COB so that a TT isn't required, only works with GhostCOB
-        if (meal_data.mealCOB > 0) eatingnow = false;
+        // High delta enable eating now
+        if (glucose_status.delta >=15 && iob_data.iob <= profile.EatingNowIOB && eatingnowtimeOK) eatingnow = true;
         // Force eatingnow mode by setting a 5.5 temp target EatingNowIOB trigger is ignored, EatingNowIOBMax is respected, max bolus is restricted if outside of allowed hours
         if (profile.temptargetSet) {  // tt duration prevents immediate SMB
             // normal target or less enables eating now
@@ -338,6 +338,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
              // any TT of normal target or below with override will allow eating now to operate outside of hours
             // if (profile.EatingNowOverride && target_bg < profile.normal_target_bg) eatingnow = true;
          }
+        // enable eating now when there are COB so that a TT isn't required, only works with GhostCOB
+        if (meal_data.mealCOB > 0) eatingnow = false;
     }
 
     // patches ===== END
