afd2bf2dcd91bcbb8bfab181bf4413ccc0f9fc40
dicko <mark@servecold.net>
2021-08-20 08:23:05 +1000

    Reinstate prebolus of EatingNowUAMBoostBolus for TT longer than 60 mins

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -954,17 +954,17 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         enableSMB = false;
     }
 
-//    // ********* EXPERIMENTAL ************
-//    // we are in a TT that is low enough and long enough and just started so allow up to 1U prebolus if SMB allowed
-//    if (eatingsoon && profile.temptarget_duration >= 181 && profile.temptarget_minutesrunning == 0) {
-//        enableSMB = true;
-//        var preBolus = Math.min((profile.temptarget_duration - 180)/10,1.5);;
-//        rT.units = preBolus;
-//        rT.insulinReq = preBolus;
-//        rT.reason += "Eating soon " + profile.temptarget_duration + "m +" + preBolus + "U. ";
-//        return rT;
-//    }
-//    // ********* EXPERIMENTAL ************
+    // ********* EXPERIMENTAL ************
+    // we are in a TT that is low enough and long enough and just started so allow a prebolus
+    if (eatingnow && profile.temptarget_duration > 60 && profile.temptarget_minutesrunning == 0) {
+        enableSMB = true;
+        var preBolus = profile.EatingNowUAMBoostBolus; //Math.min((profile.temptarget_duration - 180)/10,1.5);;
+        rT.units = preBolus;
+        rT.insulinReq = preBolus;
+        rT.reason += "EN: " + profile.temptarget_duration + "m, prebolusing " + preBolus + "U. ";
+        return rT;
+    }
+    // ********* EXPERIMENTAL ************
 
     console.error("BG projected to remain above",convert_bg(min_bg, profile),"for",minutesAboveMinBG,"minutes");
     if ( minutesAboveThreshold < 240 || minutesAboveMinBG < 60 ) {
