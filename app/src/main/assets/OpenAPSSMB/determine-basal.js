3f15b26ae7a9ac2b14293b95f490053d60071d57
dicko <mark@servecold.net>
2021-08-24 12:17:07 +1000

    Relocate prebolus code

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -527,7 +527,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         if (target_bg < profile.normal_target_bg) preBolus *= 1.5;
         rT.units = preBolus;
         rT.insulinReq = preBolus;
-        rT.reason += "EN: " + profile.temptarget_duration + "m, prebolusing " + preBolus + "U. ";
+        rT.reason = "EN: " + profile.temptarget_duration + "m, prebolusing " + preBolus + "U. ";
         return rT;
     }
     // ********* EXPERIMENTAL ************
