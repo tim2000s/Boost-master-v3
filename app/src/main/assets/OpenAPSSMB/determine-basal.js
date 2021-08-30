55d5574f2c38131e6e44e787b29a8f5c2dd38003
dicko <mark@servecold.net>
2021-08-30 10:49:16 +1000

    prebolus now 80% of EatingNowUAMBoostMaxSMB

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -522,12 +522,13 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     // we are in a TT that is long enough and just started to allow a prebolus
     if (eatingnow && profile.temptarget_duration > 60 && profile.temptarget_minutesrunning == 0) {
         enableSMB = true;
-        var preBolus = profile.EatingNowUAMBoostBolus; //Math.min((profile.temptarget_duration - 180)/10,1.5);;
+        //var preBolus = profile.EatingNowUAMBoostBolus; //Math.min((profile.temptarget_duration - 180)/10,1.5);;
+        var preBolus = round(profile.EatingNowUAMBoostMaxSMB * 0.8,1);
         // allow 150% more prebolus for low TT
-        if (target_bg < profile.normal_target_bg) preBolus *= 1.5;
-        rT.units = round(preBolus,1);
-        rT.insulinReq = preBolus;
-        rT.reason = "EN: " + convert_bg(target_bg, profile) + " Temp Target for " + profile.temptarget_duration + " mins, prebolusing " + preBolus + "U. ";
+        //if (target_bg < profile.normal_target_bg) preBolus *= 1.5;
+        rT.units = preBolus;
+        rT.insulinReq = rT.units;
+        rT.reason = "EN: " + convert_bg(target_bg, profile) + " Temp Target for " + profile.temptarget_duration + " mins, prebolusing " + rT.units + "U. ";
         return rT;
     }
     // ********* EXPERIMENTAL ************
