c0e0a8366cd852dbcd51cf2dd91c69934a74b743
dicko <mark@servecold.net>
2021-09-03 13:14:51 +1000

    Allow inherited insulinReqPct with maxBolus restriction

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -1364,7 +1364,9 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
                 } else if (profile.EatingNowOverride && profile.temptargetSet) {
                     // increase maxbolus outside of hours specified with a low TT and override enabled, otherwise use maxBolus
                     maxBolus = (target_bg < profile.normal_target_bg ? EatingNowMaxSMB : maxBolus);
-                    insulinReqPct = (insulinReqPct == 0 ? 0 : insulinReqPctDefault); // need this for safety as testing
+                    //insulinReqPct = (insulinReqPct == 0 ? 0 : insulinReqPctDefault); // need this for safety as testing
+                    // Allow inherited insulinReqPct with maxBolus restriction
+                    insulinReqPct = (target_bg < profile.normal_target_bg ? insulinReqPctDefault : insulinReqPct);
                 }
 
                 // ============== INSULIN BOOST  ==============
