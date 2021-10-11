9e99f38e65f4e613be597ceff1b68c61cdc64935
dicko <mark@servecold.net>
2021-10-11 17:11:18 +1100

    prebolus text fix

--- app/src/main/assets/OpenAPSSMB/determine-basal.js
+++ app/src/main/assets/OpenAPSSMB/determine-basal.js
@@ -534,7 +534,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         rT.units = preBolus;
         rT.insulinReq = rT.units;
         rT.boostType = "Prebolus";
-        rT.reason = "EN: " + convert_bg(target_bg, profile) + " Temp Target for " + profile.temptarget_duration + " mins, prebolusing " + rT.units + "/" + profile.EatingNowUAMBoostMaxSMB + "U.";
+        rT.reason = "EN: " + convert_bg(target_bg, profile) + " Temp Target for " + profile.temptarget_duration + " mins, prebolusing " + rT.units + "/" + (profile.current_basal * profile.EatingNowUAMBoostMaxSMB / 60) + "U.";
         return rT;
     }
 
