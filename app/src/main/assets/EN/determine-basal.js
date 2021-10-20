acfb2213a3b193dbf5b22f9bf790bd43a37b9541
dicko <mark@servecold.net>
2021-10-20 20:37:19 +1100

    BolusedEN

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -426,8 +426,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         var PredAnalise = HyperPredBGTest - HyperPredBGTest2 - HyperPredBGTest3;
         var iTime = round(( new Date(systemTime).getTime() - meal_data.lastBolusCorr ) / 60000,0);
 //        var BolusedEN = round((new Date().setHours(profile.EatingNowTimeStart) - meal_data.lastBolusCorr )/60000,0);
-        var BolusedEN = new Date(systemTime).setHours(profile.EatingNowTimeStart,0,0);
-//        BolusedEN.setHours(7,0,0);
+        var BolusedEN = new Date(systemTime).setHours(profile.EatingNowTimeStart,0,0,0);
         console.log("BolusedEN: "+ BolusedEN);
 
         var csf = profile.sens / profile.carb_ratio; // DO WE NEED THIS?
