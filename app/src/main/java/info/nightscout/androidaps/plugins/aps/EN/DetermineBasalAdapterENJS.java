de4a2a02e72856f8f22f1d1049db4ccc3e4ada62
dicko <mark@servecold.net>
2021-10-23 10:26:22 +1100

    iTime1

--- app/src/main/java/info/nightscout/androidaps/plugins/aps/EN/DetermineBasalAdapterENJS.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/aps/EN/DetermineBasalAdapterENJS.java
@@ -404,7 +404,7 @@ public class DetermineBasalAdapterENJS {
         mMealData.put("lastBolusTime", mealData.lastBolusTime);
         mMealData.put("lastCarbTime", mealData.lastCarbTime);
         mMealData.put("lastBolusCorr",treatmentsPlugin.getLastBolusTime(true));
-        mMealData.put("firstENBolusTime",treatmentsPlugin.getFirstENBolusTime(sp.getInt(R.string.key_eatingnow_timestart, 9)));
+        mMealData.put("firstBolusCorr",treatmentsPlugin.getFirstENBolusTime(sp.getInt(R.string.key_eatingnow_timestart, 9)));
 
         tddAIMI = new TddCalculator(hasAndroidInjector,aapsLogger,rxBusWrapper,resourceHelper,mainApp,sp,activePluginProvider,profileFunction,fabricPrivacy,nsUpload,dateUtil,uploadQueue);
         mMealData.put("TDDAIMI7",tddAIMI.averageTDD(tddAIMI.calculate(7)).total);
