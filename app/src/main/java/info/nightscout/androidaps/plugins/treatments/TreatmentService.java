6f0548b0f7472a8d410d7cdc772482fe221d42e2
dicko <mark@servecold.net>
2021-10-23 08:34:11 +1100

    firstENBolusTime

--- app/src/main/java/info/nightscout/androidaps/plugins/treatments/TreatmentService.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/treatments/TreatmentService.java
@@ -686,7 +686,7 @@ public class TreatmentService extends OrmLiteBaseService<DatabaseHelper> {
             where.and().ge("date", DateUtil.toTodayTime(ENStartTime));
             where.and().eq("isValid", true);
             where.and().eq("isSMB", false);
-            queryBuilder.orderBy("date", true);
+            queryBuilder.orderBy("date", false);
             queryBuilder.limit(1L);
 
             List<Treatment> result = getDao().query(queryBuilder.prepare());
