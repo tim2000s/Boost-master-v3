f814ac2942f3226455db44455780b52e0f6858fa
dicko <mark@servecold.net>
2021-10-24 14:21:47 +1100

    UAMBoost text cleanup

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1553,7 +1553,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
                 rT.reason += "; setting " + durationReq + "m low temp of " + smbLowTempReq + "U/h";
             }
             rT.reason += ". ";
-            rT.reason += UAMBoostReason + "*"; // THE END?
+            rT.reason += UAMBoostReason + "* "; // THE END?
             rT.reason = esc_text(rT.reason);
 
             //allow SMBs every 3 minutes by default
