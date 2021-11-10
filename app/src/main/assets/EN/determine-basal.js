72746c6b92e4b8395e7ee627c7f4f79bb65469ed
dicko <mark@servecold.net>
2021-11-11 10:03:28 +1100

    cTime bolusing message rounding CR

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -634,7 +634,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         rT.units = preBolus;
         rT.insulinReq = rT.units;
         rT.boostType = "Prebolus";
-        rT.reason = esc_text(meal_data.lastCarbs +"g COB " + cTime + "m ago, CR:"+ profile.carb_ratio+" Bolusing " + round(preBolusPct*100) + "% = " + rT.units + "U");
+        rT.reason = esc_text(meal_data.lastCarbs +"g COB " + cTime + "m ago, CR:"+ round(profile.carb_ratio)+" Bolusing " + round(preBolusPct*100) + "% = " + rT.units + "U");
         return rT;
     }
     console.log ("cTime:"+cTime+", iTime:"+iTime+",lastCarbs:"+meal_data.lastCarbs);
