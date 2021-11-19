404b4a58afe2d82bf4880ecfdcce392909333e4e
dicko <mark@servecold.net>
2021-11-20 08:17:07 +1100

    fixup! restrict sensitivityRatio to 1 max when autoISF is not being used

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -265,6 +265,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         sensitivityRatio = sensitivityRatio * autosens_data.ratio; //now apply existing sensitivity or resistance
         // limit sensitivityRatio to profile.autosens_max
         sensitivityRatio = Math.min(sensitivityRatio, profile.autosens_max);
+        // restrict SR to 1 max if not using autoISF hence variable_sens may help with overnight low allowing basal to be adjusted
+        sensitivityRatio = (!profile.use_autoisf ? Math.min(sensitivityRatio,1) : sensitivityRatio);
         sensitivityRatio = round(sensitivityRatio,2);
         console.log("Sensitivity ratio set to "+sensitivityRatio+" based on temp target of "+target_bg+"; ");
     } else if (typeof autosens_data !== 'undefined' && autosens_data) {
