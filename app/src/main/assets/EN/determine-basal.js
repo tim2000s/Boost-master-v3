833a7df21a5228e21c13ac348ada9c96f62dbffd
dicko <mark@servecold.net>
2021-11-23 22:21:57 +1100

    EM: inactive

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1345,7 +1345,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
             var insulinReqPctDefault = 0.65; // this is the default insulinReqPct and maxBolus is respected outside of eating now
             var insulinReqPct = insulinReqPctDefault; // this is the default insulinReqPct and maxBolus is respected outside of eating now
             var insulinReqOrig = insulinReq;
-            var UAMBoostReason = (!eatingnowtimeOK ? "EN: not started" : "EN: no boost required"); //reason text for oaps pill is nothing to start
+            var UAMBoostReason = (!eatingnowtimeOK ? "EN: inactive" : "EN: no boost required"); //reason text for oaps pill is nothing to start
             var insulinReqBoost = 0; // no boost yet
             var EatingNowMaxSMB = maxBolus;
             var UAMBoosted = false, ISFBoosted = false, UAMBoostMAX = false;
