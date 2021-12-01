0c8b81cdf78ff5dccb2add111d7050614535a779
dicko <mark@servecold.net>
2021-12-01 12:28:09 +1100

    if COB allow increase UAMBoost max SMB * EXPERIMENTAL *

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1409,8 +1409,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
                     EatingNowMaxSMB = ( EatingNowMaxSMB > 0 ? EatingNowMaxSMB : maxBolus );
                     // recent SMB that was bigger than maxBolus restrict next UAM SMB size
                     EatingNowMaxSMB = ( SMBTime <=7 && meal_data.lastSMBUnits > maxBolus ? maxBolus : EatingNowMaxSMB);
-                    // if COB allow increase UAMBoost max SMB * EXPERIMENTAL *
-                    //EatingNowMaxSMB = (meal_data.mealCOB > 0 && !ignoreCOBPatch ? (meal_data.mealCOB / profile.carb_ratio) * profile.EatingNowPrebolusPct : EatingNowMaxSMB);
+                    // if COB allow increase UAMBoost max SMB if Carbs were less than an hour ago
+                    EatingNowMaxSMB = Math.max((meal_data.mealCOB > 0 && cTime < 60 && !ignoreCOBPatch ? (meal_data.mealCOB / profile.carb_ratio) * profile.EatingNowPrebolusPct : EatingNowMaxSMB),EatingNowMaxSMB);
                     EatingNowMaxSMB = round (EatingNowMaxSMB,1);
                     // allow 100% insulinReqPct when initial rise is known to go higher EatingNowBGThreshold and not high already
                     insulinReqPct = (eventualBG > EatingNowBGThreshold && bg < EatingNowBGThreshold ? 1 : insulinReqPct);
