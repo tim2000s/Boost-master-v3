14a9ffe0001ca2970b9bde6119e80f46e3743283
dicko <mark@servecold.net>
2021-12-05 09:49:14 +1100

    UAMBoostReason status

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1356,7 +1356,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
             var insulinReqPctDefault = 0.65; // this is the default insulinReqPct and maxBolus is respected outside of eating now
             var insulinReqPct = insulinReqPctDefault; // this is the default insulinReqPct and maxBolus is respected outside of eating now
             var insulinReqOrig = insulinReq;
-            var UAMBoostReason = (!eatingnowtimeOK ? "EN: inactive" : "EN: no boost"); //reason text for oaps pill is nothing to start
+            var UAMBoostReason = (!eatingnow ? "EN: inactive" : "EN: no boost"); //reason text for oaps pill is nothing to start
             UAMBoostReason = (!iTimeOK ? "EN: not iTime" : "EN: no boost"); //reason text for oaps pill is nothing to start
             var insulinReqBoost = 0; // no boost yet
             var EatingNowMaxSMB = maxBolus, EatingNowMaxCOBSMB = 0;
