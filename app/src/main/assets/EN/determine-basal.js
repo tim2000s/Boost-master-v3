3dbbad0dc189e6c2aff254bb221d93aca9ca3fb7
DICKO <mark@servecold.net>
2022-01-16 09:32:14 +1100

    TDD code tweak to simplify

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -380,19 +380,18 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     var TDDReason="";
     //console.error("Pump extrapolated TDD = "+tdd_pump+"; ");
     enlog += "Pump extrapolated TDD:"+round(tdd_pump,3)+"\n";
+
     if (!eatingnowtimeOK && !iTimeOverride) {
+        // overnight just use the average as pump extrapolation can vary after midnight
         TDD = tdd_avg;
         TDDReason = "Night";
-    } else if (tdd_pump < (0.5 * tdd_avg) && TIRBelow < 1){
-        TDD = (tdd_avg * 0.2) + (tdd_pump * 0.8);
+    } else if (tdd_pump < (0.5 * tdd_avg)){
+        // low insulin usage during EN hours
+        TDD = (tdd_avg * 0.25) + (tdd_pump * 0.75);
         TDDReason = "Pump";
         enlog += "TDD weighted to pump due to low insulin usage. TDD:"+round(TDD,3)+"\n";
-    } else if (tdd_pump > (1.3 * tdd_avg) && TIRAbove==1) {
-        TDD = (tdd_avg * 0.8) + (tdd_pump * 0.2);
-        //console.error("TDD weighted to TDD7 due to high insulin usage. TDD = "+TDD+"; ");
-        TDDReason = "Avg";
-        enlog += "TDD weighted to 80% TDDAVG due to high insulin usage. TDDAVG:"+round(tdd_avg,3)+", TDD:"+round(TDD,3)+"\n";
     } else {
+        // no weighting change
         enlog +="TDDAVG:"+round(tdd_avg,3)+", TDD Pump:"+round(tdd_pump,3)+" and TDD:"+round(TDD,3)+"\n";
     }
 
