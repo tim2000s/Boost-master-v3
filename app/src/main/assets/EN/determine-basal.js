c18db98324199be4ebaf9989aaf64ccbb465f82b
DICKO <mark@servecold.net>
2022-01-16 15:08:31 +1100

    limit sens_future to ISF_Max with no iTimeOverride or iTime

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -865,9 +865,10 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
     // if BG below threshold then take the max of the sens vars
     sens_future = (bg <= threshold ? Math.max(sens_normalTarget, sens_currentBG, sens_future) : sens_future);
-    // limit sens_future to ISF_Max with no iTimeOverride and if limited set sens_future_max to true for reason
-    if (!iTimeOverride) {
+    // limit sens_future to ISF_Max with no iTimeOverride or COBBoost
+    if (!iTimeOverride || !iTime ) {
         sens_future = Math.max(sens_future, ISF_Max);
+        // set sens_future_max to true for reason asterisk
         sens_future_max = (sens_future == ISF_Max);
     }
 
