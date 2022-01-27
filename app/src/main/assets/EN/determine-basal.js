dc5d0d318b6fae1a924d5dcef8fdb3c8e39a0268
DICKO <mark@servecold.net>
2022-01-28 08:43:23 +1100

    sens_eBGweight comments

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -847,7 +847,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         // favour eventualBG more due to delta based on the sens_predType using sens_eBGweight
         // scale sens_eBGweight based on delta with a max for each prediction type
         sens_eBGweight = (sens_predType=="COB" ? Math.min(glucose_status.delta*.15,0.75) : sens_eBGweight); // 15% increments max 75%
-        sens_eBGweight = (sens_predType=="UAM" ? Math.min((glucose_status.delta*.03)+0.10,0.55) : sens_eBGweight); // 5% increments max 65% starting at 10%
+        sens_eBGweight = (sens_predType=="UAM" ? Math.min((glucose_status.delta*.03)+0.10,0.55) : sens_eBGweight); // 3% increments max 55% starting at 10%
         // eventualBG lower than current BG
         sens_eBGweight = (eventualBG < bg ? 1 : sens_eBGweight);
         // allow any rise to use COB sens_eBGweight for COBBoostOK
