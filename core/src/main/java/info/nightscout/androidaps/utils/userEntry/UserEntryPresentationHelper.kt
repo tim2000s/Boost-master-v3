161a0a8f73b62dbb0609667a2678e7c03b71ec67
piecycle <pinner.michael@gmail.com>
2022-02-12 11:31:54 +0100

    Add UAM button for further dev

--- core/src/main/java/info/nightscout/androidaps/utils/userEntry/UserEntryPresentationHelper.kt
+++ core/src/main/java/info/nightscout/androidaps/utils/userEntry/UserEntryPresentationHelper.kt
@@ -42,7 +42,8 @@ class UserEntryPresentationHelper @Inject constructor(
 
     fun iconId(source: Sources): Int = when (source) {
         Sources.TreatmentDialog     -> R.drawable.icon_insulin_carbs
-        Sources.InsulinDialog       -> R.drawable.ic_bolus
+        Sources.InsulinDialog       -> R.drawable.ic_bolus //MP Button test
+        Sources.UAMDialog           -> R.drawable.icon_insulin_carbs
         Sources.CarbDialog          -> R.drawable.ic_cp_bolus_carbs
         Sources.WizardDialog        -> R.drawable.ic_calculator
         Sources.QuickWizard         -> R.drawable.ic_quick_wizard
