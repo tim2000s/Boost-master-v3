baca3915530f5ea497b6886f9e016dd9c355ca2f
t_j_s <street.tj@gmail.com>
2022-03-08 13:46:01 +0000

    ALlow autosens to use rolling 24 hour and compare to last 7 days average.

--- app/src/main/assets/Boost/determine-basal.js
+++ app/src/main/assets/Boost/determine-basal.js
@@ -399,7 +399,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         console.log("ISF from "+variable_sens+" to "+sens+ "due to temp target; ");
         }
         else {
-        sensitivityRatio = ( TDD / tdd1 );
+        sensitivityRatio = ( tdd_24 / tdd7 );
             if (sensitivityRatio > 1) {
             sensitivityRatio = Math.min(sensitivityRatio, profile.autosens_max);
             sensitivityRatio = round(sensitivityRatio,2);
