885e0af4a33bc695835ea5354cc1bff66721559d
piecycle <pinner.michael@gmail.com>
2022-03-12 13:52:37 +0100

    Pure smoothing code, rearranged

--- core/src/main/java/info/nightscout/androidaps/plugins/iob/iobCobCalculator/GlucoseStatus.kt
+++ core/src/main/java/info/nightscout/androidaps/plugins/iob/iobCobCalculator/GlucoseStatus.kt
@@ -9,14 +9,22 @@ data class GlucoseStatus(
     val delta: Double = 0.0,
     val shortAvgDelta: Double = 0.0,
     val longAvgDelta: Double = 0.0,
-    val date: Long = 0L
+    val date: Long = 0L,
+    //*** Tsunami data smoothing ***
+    var insufficientSmoothingData: Boolean = false,
+    var ssBGnow: Double = 0.0,
+    var ssDnow: Double = 0.0,
 ) {
 
     fun log(): String = "Glucose: " + DecimalFormatter.to0Decimal(glucose) + " mg/dl " +
         "Noise: " + DecimalFormatter.to0Decimal(noise) + " " +
         "Delta: " + DecimalFormatter.to0Decimal(delta) + " mg/dl" +
         "Short avg. delta: " + " " + DecimalFormatter.to2Decimal(shortAvgDelta) + " mg/dl " +
-        "Long avg. delta: " + DecimalFormatter.to2Decimal(longAvgDelta) + " mg/dl"
+        "Long avg. delta: " + DecimalFormatter.to2Decimal(longAvgDelta) + " mg/dl" +
+        //*** Tsunami data smoothing ***
+        "insufficientSmoothingData: " + insufficientSmoothingData +
+        "ssBGnow: " + DecimalFormatter.to0Decimal(ssBGnow) + " mg/dl " +
+        "ssDnow: " + DecimalFormatter.to0Decimal(ssDnow) + " mg/dl "
 }
 
 fun GlucoseStatus.asRounded() = copy(
@@ -24,5 +32,8 @@ fun GlucoseStatus.asRounded() = copy(
     noise = Round.roundTo(noise, 0.01),
     delta = Round.roundTo(delta, 0.01),
     shortAvgDelta = Round.roundTo(shortAvgDelta, 0.01),
-    longAvgDelta = Round.roundTo(longAvgDelta, 0.01)
+    longAvgDelta = Round.roundTo(longAvgDelta, 0.01),
+    //*** Tsunami data smoothing ***
+    ssBGnow = Round.roundTo(ssBGnow, 0.1),
+    ssDnow = Round.roundTo(ssDnow, 0.1),
 )
