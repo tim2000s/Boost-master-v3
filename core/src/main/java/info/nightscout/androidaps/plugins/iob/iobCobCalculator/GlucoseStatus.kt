10dd2a12ef710f4d3b2001bf82180df801821b5c
piecycle <pinner.michael@gmail.com>
2022-05-07 23:01:51 +0200

    update to latest dev

--- core/src/main/java/info/nightscout/androidaps/plugins/iob/iobCobCalculator/GlucoseStatus.kt
+++ core/src/main/java/info/nightscout/androidaps/plugins/iob/iobCobCalculator/GlucoseStatus.kt
@@ -11,10 +11,7 @@ data class GlucoseStatus(
     val longAvgDelta: Double = 0.0,
     val date: Long = 0L,
     //*** Tsunami ***
-    var activity_pred_time: Long = 40L, //MP Time in minutes from now to calculate insulin activity for
     var deltaScore: Double = 0.0,
-    var deltathreshold: Double = 7.0, //MP average delta above which deltaScore will be 1.
-    var weight: Double = 0.15 //MP Weighting used for weighted averages
 ) {
 
     fun log(): String = "Glucose: " + DecimalFormatter.to0Decimal(glucose) + " mg/dl " +
@@ -23,9 +20,7 @@ data class GlucoseStatus(
         "Short avg. delta: " + " " + DecimalFormatter.to2Decimal(shortAvgDelta) + " mg/dl " +
         "Long avg. delta: " + DecimalFormatter.to2Decimal(longAvgDelta) + " mg/dl" +
         //*** Tsunami ***
-        "activity_pred_time: " + activity_pred_time + " min" +
-        "deltaScore: " + DecimalFormatter.to2Decimal(deltaScore) + " a.u." +
-        "Long avg. delta: " + DecimalFormatter.to2Decimal(longAvgDelta) + " mg/dl"
+        "deltaScore: " + DecimalFormatter.to2Decimal(deltaScore) + " a.u."
 }
 
 fun GlucoseStatus.asRounded() = copy(
